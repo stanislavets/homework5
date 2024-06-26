9dc43972b7dbd407c6ae20136343ab2c521360d4
stanislavets <stanislavets2009@mail.ru>
2024-06-26 20:44:39 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var=(1,2,3,"строка", True)
+print("immutable_var:", immutable_var)
+#tuple[1]=150
+mutable_list=["первый", "второй", "третий", 1,2,3]
+mutable_list[0]="пятый"
+print("mutable_list:", mutable_list)
